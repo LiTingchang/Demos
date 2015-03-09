44c5eeccdcd8cbf78bfdd9ee0087b522dccc7b07
Li TingChang <litingchang@jd.com>
2015-03-09 18:14:24 +0800

    Park ui res

---
+++ Park/src/com/park/MainActivity.java
@@ -0,0 +1,32 @@
+
+package com.park;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    
+    int [] views = {R.id.temp1, R.id.temp2, R.id.temp3,
+            R.id.temp4, R.id.temp5, R.id.temp6,};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        for(int i = 0; i < views.length;  ++i) {
+            findViewById(views[i]).setOnClickListener(new View.OnClickListener() {
+                
+                @Override
+                public void onClick(View v) {
+                    // TODO Auto-generated method stub
+                    
+                }
+            });
+        }
+        
+    }
+}
