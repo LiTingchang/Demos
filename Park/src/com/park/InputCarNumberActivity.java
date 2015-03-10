23819f32c8e0146a719181556898ad95efd96693
Li TingChang <litingchang@jd.com>
2015-03-10 22:43:04 +0800

    输入法与切换tab整合

--- Park/src/com/park/InputCarNumberActivity.java
+++ Park/src/com/park/InputCarNumberActivity.java
@@ -3,56 +3,101 @@ package com.park;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.park.bean.InputChar;
-import com.park.utils.ToastUtil;
-import com.park.view.InputPanel;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.widget.Toast;
+import android.text.InputType;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import com.park.bean.InputChar;
+import com.park.view.InputPanel;
 
 public class InputCarNumberActivity extends FragmentActivity{
     
+    private InputPanel inputPanel1;
+    private InputPanel inputPanel2;
+    private InputPanel inputPanel3;
+    private InputPanel inputPanel4;
     
-    InputPanel inputPanel;
-    InputPanel inputPanel2;
-    InputPanel inputPanel3;
-    InputPanel inputPanel4;
+    private View inputContainerPanel23;
+    private View inputContainerPanel4;
+    
+    private RadioButton radioBtn1;
+    private RadioButton radioBtn2;
+    private RadioButton radioBtn3;
+    
+    private EditText inputPlateNumber;
+    private View generateOrder;
+    
+    private StringBuffer plateNumer;
     
     @Override
     protected void onCreate(Bundle arg0) {
-        // TODO Auto-generated method stub
         super.onCreate(arg0);
         
+        plateNumer = new StringBuffer();
         
         setContentView(R.layout.activity_input_car_number);
         
+        inputPanel1 = (InputPanel) findViewById(R.id.input_panel1);
+        inputPanel2 = (InputPanel) findViewById(R.id.input_panel2);
+        inputPanel3 = (InputPanel) findViewById(R.id.input_panel3);
+        inputPanel4 = (InputPanel) findViewById(R.id.input_panel4);
+        inputContainerPanel23 = findViewById(R.id.input_panel23_container);
+        inputContainerPanel4 = findViewById(R.id.input_panel4_container);
         
+        radioBtn1 = (RadioButton) findViewById(R.id.input_tab_1);
+        radioBtn2 = (RadioButton) findViewById(R.id.input_tab_2);
+        radioBtn3 = (RadioButton) findViewById(R.id.input_tab_3);
+        
+        generateOrder = findViewById(R.id.generate_order);
+        
+        //  省份
+        String [] str = {"京", "津", "沪", "渝", "冀", "豫", "云", "辽", "黑", 
+                "湘", "皖", "鲁", "新", "苏", "浙", "赣", "鄂", "桂", "甘", "晋", 
+                "蒙", "陕", "吉", "闽", "贵", "粤", "青", "藏", "川", "宁", "琼"};  
         List<InputChar> inputChars = new ArrayList<InputChar>();
+        for(int i = 0; i < str.length; ++i) {
+            InputChar inputChar = new InputChar();
+            inputChar.id = i;
+            inputChar.content = str[i];
+            inputChars.add(inputChar);
+        }
+        inputPanel1.init(inputChars, R.layout.view_input_line);
+        inputPanel1.setOnItemClickListener(new InputPanel.OnItemClickListener() {
+            
+            @Override
+            public void onItemClick(int position, InputChar inputChar) {
+                appendPlateNumber(inputChar);
+            }
+        });
+        
+        
+        // 字母 
+        List<InputChar> inputChars1 = new ArrayList<InputChar>();
         char ch = 'A';
         for(int i = 0; i <= 25; ++i) {
             InputChar inputChar = new InputChar();
             inputChar.id = i;
             inputChar.content = String.valueOf((char)(ch + i));
-            inputChars.add(inputChar);
+            inputChars1.add(inputChar);
         }
         
-        inputPanel = (InputPanel) findViewById(R.id.input_panel);
-        inputPanel.init(inputChars, R.layout.view_input_char , 5);
-        
-        inputPanel.setOnItemClickListener(new InputPanel.OnItemClickListener() {
+        inputPanel2.init(inputChars1, R.layout.view_input_char , 5);
+        inputPanel2.setOnItemClickListener(new InputPanel.OnItemClickListener() {
             
             @Override
             public void onItemClick(int position, InputChar inputChar) {
-                // TODO Auto-generated method stub
-                ToastUtil.shortToast(getApplicationContext(), inputChar.content);
+                appendPlateNumber(inputChar);
             }
         });
         
         
-        
+        // 数字
         List<InputChar> inputChars2 = new ArrayList<InputChar>();
         for(int i = 0; i <= 9; ++i) {
             InputChar inputChar = new InputChar();
@@ -60,64 +105,105 @@ public class InputCarNumberActivity extends FragmentActivity{
             inputChar.content = (i+1)%10 + "";
             inputChars2.add(inputChar);
         }
-        
-        inputPanel2 = (InputPanel) findViewById(R.id.input_panel2);
-        inputPanel2.init(inputChars2, R.layout.view_input_number, 5);
-        
-        inputPanel2.setOnItemClickListener(new InputPanel.OnItemClickListener() {
-            
-            @Override
-            public void onItemClick(int position, InputChar inputChar) {
-                // TODO Auto-generated method stub
-                ToastUtil.shortToast(getApplicationContext(), inputChar.content);
-            }
-        });
-        
-        String [] str = {"京", "津", "沪", "渝", "冀", "豫", "云", "辽", "黑", 
-                "湘", "皖", "鲁", "新", "苏", "浙", "赣", "鄂", "桂", "甘", "晋", 
-                "蒙", "陕", "吉", "闽", "贵", "粤", "青", "藏", "川", "宁", "琼"};  
-        List<InputChar> inputChars3 = new ArrayList<InputChar>();
-        for(int i = 0; i < str.length; ++i) {
-            InputChar inputChar = new InputChar();
-            inputChar.id = i;
-            inputChar.content = str[i];
-            inputChars3.add(inputChar);
-        }
-        
-        inputPanel3 = (InputPanel) findViewById(R.id.input_panel3);
-        inputPanel3.init(inputChars3, R.layout.view_input_line);
-        
+        inputPanel3.init(inputChars2, R.layout.view_input_number, 5);
         inputPanel3.setOnItemClickListener(new InputPanel.OnItemClickListener() {
             
             @Override
             public void onItemClick(int position, InputChar inputChar) {
-                // TODO Auto-generated method stub
-                ToastUtil.shortToast(getApplicationContext(), inputChar.content);
+                appendPlateNumber(inputChar);
             }
         });
         
-        String [] str2 = {"京", "津", "沪", "渝", "冀", "豫", "云", "辽", "黑", 
-                "湘", "皖", "鲁", "新", "苏", "浙", "赣", "鄂", "桂", "甘", "晋", 
-                "蒙", "陕", "吉", "闽", "贵", "粤", "青", "藏", "川", "宁", "琼"};  
+        
+        // 特殊牌照
+        String [] str2 = {"军", "空", "海", "北", "沈", "兰", "济", "南", "广", 
+                "成", "WJ", "边", "电", "金", "警", "森", "通", "消"};  
         List<InputChar> inputChars4 = new ArrayList<InputChar>();
         for(int i = 0; i < str2.length; ++i) {
             InputChar inputChar = new InputChar();
             inputChar.id = i;
-            inputChar.content = str[i];
-            inputChars3.add(inputChar);
+            inputChar.content = str2 [i];
+            inputChars4.add(inputChar);
         }
         
-        inputPanel4 = (InputPanel) findViewById(R.id.input_panel4);
         inputPanel4.init(inputChars4, R.layout.view_input_line);
-        
         inputPanel4.setOnItemClickListener(new InputPanel.OnItemClickListener() {
             
             @Override
             public void onItemClick(int position, InputChar inputChar) {
-                // TODO Auto-generated method stub
-                ToastUtil.shortToast(getApplicationContext(), inputChar.content);
+                appendPlateNumber(inputChar);
             }
         });
+        
+        
+        radioBtn1.setOnCheckedChangeListener(new RadioButton.OnCheckedChangeListener() {
+            
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    inputPanel1.setVisibility(View.VISIBLE);
+                    inputContainerPanel23.setVisibility(View.GONE);
+                    inputContainerPanel4.setVisibility(View.GONE);
+                }
+            }
+        });
+        
+        radioBtn2.setOnCheckedChangeListener(new RadioButton.OnCheckedChangeListener() {
+            
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    inputPanel1.setVisibility(View.GONE);
+                    inputContainerPanel23.setVisibility(View.VISIBLE);
+                    inputContainerPanel4.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        radioBtn3.setOnCheckedChangeListener(new RadioButton.OnCheckedChangeListener() {
+            
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    inputPanel1.setVisibility(View.GONE);
+                    inputContainerPanel23.setVisibility(View.GONE);
+                    inputContainerPanel4.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        
+        radioBtn1.setChecked(true);
+        
+        
+        
+        inputPlateNumber = (EditText) findViewById(R.id.input_plate_number);
+        // 设置不接收系统软件盘输入
+        inputPlateNumber.setInputType(InputType.TYPE_NULL);
+        
+//        inputPlateNUmber.setInputType(InputType.TYPE_CLASS_TEXT);
+        
+        
+        generateOrder.setOnClickListener(new View.OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                
+            }
+        });
+    }
+    
+    private void appendPlateNumber(InputChar inputChar) {
+        plateNumer.append(inputChar.content);
+        
+        inputPlateNumber.setText(plateNumer.toString());
+    }
+    
+    private void removeEndPlateNumber() {
+        int length = plateNumer.length();
+        plateNumer.delete(length - 1, length);
+        
+        inputPlateNumber.setText(plateNumer.toString());
     }
     
     
@@ -125,5 +211,4 @@ public class InputCarNumberActivity extends FragmentActivity{
         Intent intent = new Intent(activity, InputCarNumberActivity.class);
         activity.startActivity(intent);
     }
-
 }
--- Park/src/com/park/MainActivity.java
+++ Park/src/com/park/MainActivity.java
@@ -6,30 +6,48 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Switch;
 
-public class MainActivity extends Activity {
-    
-    int [] views = {R.id.temp1, R.id.temp2, R.id.temp3,
-            R.id.temp4, R.id.temp5, R.id.temp6,};
-    
-    
-//    editText.setInputType(InputType.TYPE_NULL);
+public class MainActivity extends Activity implements OnClickListener {
+
+    int[] views = {
+            R.id.temp1, R.id.temp2, R.id.temp3,
+            R.id.temp4, R.id.temp5, R.id.temp6,
+    };
+
+    // editText.setInputType(InputType.TYPE_NULL);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        for(int i = 0; i < views.length;  ++i) {
-            findViewById(views[i]).setOnClickListener(new View.OnClickListener() {
-                
-                @Override
-                public void onClick(View v) {
-                    // TODO Auto-generated method stub
-                    InputCarNumberActivity.launch(MainActivity.this);
-                }
-            });
+
+        for (int i = 0; i < views.length; ++i) {
+            findViewById(views[i]).setOnClickListener(this);
         }
-        
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        switch (v.getId()) {
+            case R.id.temp1:
+                InputCarNumberActivity.launch(MainActivity.this);
+                break;
+            case R.id.temp2:
+
+                break;
+            case R.id.temp3:
+
+                break;
+            case R.id.temp4:
+
+                break;
+
+            default:
+                break;
+        }
     }
 }
