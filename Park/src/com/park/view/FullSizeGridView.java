36f038eaabe48a4c79814906c983c972154207c5
Li TingChang <litingchang@jd.com>
2015-03-10 10:07:36 +0800

    删除v7lib依赖

---
+++ Park/src/com/park/view/FullSizeGridView.java
@@ -0,0 +1,33 @@
+package com.park.view;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+public class FullSizeGridView extends GridView {
+
+	public FullSizeGridView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		setSelector(new ColorDrawable(Color.TRANSPARENT));
+	}
+
+	public FullSizeGridView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		setSelector(new ColorDrawable(Color.TRANSPARENT));
+	}
+
+	public FullSizeGridView(Context context) {
+		super(context);
+		setSelector(new ColorDrawable(Color.TRANSPARENT));
+	}
+
+	@Override
+	public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
+				MeasureSpec.AT_MOST);
+		
+		super.onMeasure(widthMeasureSpec, expandSpec);
+	}
+}
